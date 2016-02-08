630204121e7e274374db210d1c741ea3f8ed0b1a
Serg <seriu_1995@list.ru>
2016-02-08 16:05:52 +0200

    include bootstrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,13 +5,7 @@ module.exports = function(grunt) {
       install: {
          //just run 'grunt bower:install' and you'll see files from your Bower packages in lib directory 
         options: {
-          targetDir: './public',
-          layout: 'byType',
-          install: true,
-          verbose: false,
-          cleanTargetDir: false,
-          cleanBowerDir: false,
-          bowerOptions: {}
+          targetDir: './public'
         }
       }
     }
