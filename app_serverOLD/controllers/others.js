8da36d712d5ee6d9bd95a7730f87b461a097338c
Serg <seriu_1995@list.ru>
2016-02-16 12:13:21 +0200

    remove old server folder

--- app_serverOLD/controllers/others.js
+++
@@ -1,7 +0,0 @@
-/* GET home page */
-module.exports.about = function(req, res){
-	res.render('generic-text', { 
-		title: 'Express About',
-		content: 'Loc8r was created to help people find places to sit down and get a bit of work done. <br /><br /> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan.'
-	});
-};
--- app_serverOLD/routes/main.js
+++
@@ -1,5 +0,0 @@
-var ctrl = require('../app_server/controllers/main'); 
-
-module.exports = function (app) { 
-	app.get('/', ctrl.index);
-};
